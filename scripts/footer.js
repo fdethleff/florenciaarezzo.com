35b02e328fb50e0c3b3d13be23d3dceaab66463d
Finn Dethleff <fdethleff@gmail.com>
2022-05-29 18:30:15 +0200

    new footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,28 +1,7 @@
 const footer = Vue.createApp({
   data() {
     return {
-      windowWidth: window.innerWidth,
-      isMobileViewport: false,
     };
-  },
-  methods: {
-    handleResize() {
-      this.windowWidth = window.innerWidth;
-      if (this.windowWidth < 768) {
-        this.isMobileViewport = true;
-      } else {
-        this.isMobileViewport = false;
-      }
-    },
-  },
-  mounted() {
-    window.addEventListener("resize", this.handleResize);
-    if (this.windowWidth < 768) {
-      this.isMobileViewport = true;
-    }
-  },
-  unmounted() {
-    window.removeEventListener("resize", this.handleResize);
   },
 });
 
