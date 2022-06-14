621fb297e2e52ee965e0f503df31f18d228b3234
Finn Dethleff <fdethleff@gmail.com>
2022-06-14 13:28:24 +0200

    Add new logic to prevent early scroll

--- main.js
+++ main.js
@@ -2,16 +2,14 @@ window.addEventListener("scroll", function () {
   let hero = document.getElementById("hero");
   let blackout = document.getElementById("blackout");
   let content = document.getElementById("content");
-  if (window.scrollY > 50) {
+  if (window.scrollY > 10) {
     hero.style.transform = `translateY(-${window.innerHeight}px)`;
     blackout.style.opacity = 0;
+    let dynamicTopValue = Math.min(window.innerHeight / 5, window.scrollY);
+    content.style.top = `${dynamicTopValue}px`;
   } else {
     hero.style.transform = "translateY(0)";
     blackout.style.opacity = 1;
-  }
-  if (window.scrollY > 200) {
-    let scrollY = Math.min(200, window.scrollY);
-    content.style.top = ` ${scrollY}px`;
   }
 });
 
@@ -20,7 +18,7 @@ window.onload = function () {
   let heroVideo = document.getElementById("hero-video");
   let blackout = document.getElementById("blackout");
 
-  blackout.style.height = `${window.innerHeight * 2.25}px`;
+  blackout.style.height = `${window.innerHeight * 2.33}px`;
 
   if (window.innerWidth < 768) {
     heroVideo.setAttribute(
