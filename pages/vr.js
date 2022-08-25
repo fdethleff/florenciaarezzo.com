317d57ddba71e12eac004dacd8bdced7f2c213af
Finn Dethleff <fdethleff@gmail.com>
2022-08-25 13:13:52 +0200

    .

--- pages/vr.js
+++ pages/vr.js
@@ -140,7 +140,7 @@ function init() {
         setTimeout(function () {
             document.getElementById('ProgressBarCanvas').classList.remove('visible');
             document.getElementById('ProgressBarCanvas').classList.add('hidden');
-        }, 3000);
+        }, 4000);
 
         
     });
@@ -169,7 +169,7 @@ function init() {
         setTimeout(function () {
             document.getElementById('ProgressBarCanvas').classList.remove('visible');
             document.getElementById('ProgressBarCanvas').classList.add('hidden');
-        }, 3000);
+        }, 4000);
     });
 
     document.getElementById('reflective').addEventListener('click', function () {
@@ -195,7 +195,7 @@ function init() {
         setTimeout(function () {
             document.getElementById('ProgressBarCanvas').classList.remove('visible');
             document.getElementById('ProgressBarCanvas').classList.add('hidden');
-        }, 3000);
+        }, 4000);
     });
 
 
