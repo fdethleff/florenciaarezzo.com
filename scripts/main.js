a5e43f136cdbbe56d4268985678cb7ac6494a156
Finn Dethleff <fdethleff@gmail.com>
2022-09-29 19:34:17 +0200

    replace resized assets

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,20 @@
 window.onload = function () {
   let heroVideoDirectory = "assets/videos/";
   let heroVideo = document.getElementById("hero-video");
-  
+
   if (window.innerWidth < 768) {
     heroVideo.setAttribute(
       "src",
       heroVideoDirectory + "walking-animation-mobile.mp4"
     );
   } else {
-    heroVideo.setAttribute("src", heroVideoDirectory + "walking-animation.m4v");
+    heroVideo.setAttribute("src", heroVideoDirectory + "walking-animation.mp4");
   }
 
   const el = document.getElementById("loading-screen");
-  el.classList.add('hidden');
-  el.classList.remove('visible');
+  el.classList.add("hidden");
+  el.classList.remove("visible");
 };
-
 
 window.addEventListener("resize", function () {
   let heroVideo = document.getElementById("hero-video");
