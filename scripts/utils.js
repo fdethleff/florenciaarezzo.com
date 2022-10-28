9fd2cef47b286206349a9f020bfbf6a53ca32a78
Finn Dethleff <fdethleff@gmail.com>
2022-10-28 11:37:45 +0200

    add temporary fix for navbar

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,26 +1,38 @@
-
-// up arrow
-
-let upArrow = document.getElementById('btn-back-to-top');
-let downArrow = document.getElementById('btn-down-one-screen');
+let upArrow = document.getElementById("btn-back-to-top");
+let downArrow = document.getElementById("btn-down-one-screen");
 
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
-    upArrow.style.display = 'block';
-    downArrow.style.display = 'none';
-    down
+  if (
+    document.body.scrollTop > window.innerHeight ||
+    document.documentElement.scrollTop > window.innerHeight
+  ) {
+    if (upArrow) {
+      upArrow.style.display = "block";
+    }
+    if (downArrow) {
+      downArrow.style.display = "none";
+    }
   } else {
-    upArrow.style.display = 'none';
-    downArrow.style.display = 'block';
+    if (upArrow) {
+      upArrow.style.display = "none";
+    }
+    if (downArrow) {
+      downArrow.style.display = "block";
+    }
   }
 }
 
-upArrow.addEventListener('click', backToTop);
-downArrow.addEventListener('click', downOneScreen);
+if (upArrow) {
+  upArrow.addEventListener("click", backToTop);
+}
+
+if (downArrow) {
+  downArrow.addEventListener("click", downOneScreen);
+}
 
 function backToTop() {
   document.body.scrollTop = 0;
@@ -31,4 +43,3 @@ function downOneScreen() {
   document.body.scrollTop = window.innerHeight + 1;
   document.documentElement.scrollTop = window.innerHeight + 1;
 }
-
